993c2306c2fd99fdbf04bc2efb8b041eeb29858a
Martin <siepet7@gmail.com>
2013-05-23 01:39:02 +0300

    Update zad1.c

--- lab_14/zad1.c
+++ lab_14/zad1.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-
-    #define DLUGOSC 100
-
-    int  main () {
+#define DLUGOSC 100
+int  main() {
       char  napis[DLUGOSC] = "PROGRAM";
       printf("\n  %s\n\n", napis);
       return 0;
-    }
+}
